3c2d6f315ae185aeef58e42b1144ec74f60f994f
willnute <willnute@gmail.com>
2020-07-12 18:05:37 -0400

    7/12 Work
    Split into two files, put in pin numbers (can be changed later if needed), and other things.

---
+++ dbinterface.py
@@ -0,0 +1,87 @@
+from google.cloud import firestore
+from google.oauth2 import service_account
+
+
+# Initialize firebase interaction
+def db_init(day, gate):
+    cred = service_account.Credentials.from_service_account_file('./key.json')
+    db = firestore.Client(u'thegate-608c2', cred)
+    occDoc = db.collection(u'Counting').document(u'Occupancy-'+day)
+    if not occDoc.get().exists:
+        occDoc.set({
+            u'Current': 0,
+            u'CurrentWait': 0,
+            u'MaxOccupancy': db.collection(u'Parameters').document(u'Occupancy').get().get(u'Max'),
+            u'TotalIn': 0
+        })
+    gateDoc = db.collection(u'Gates').document(u'Gate-'+gate)
+    if not gateDoc.get().exists:
+        gateDoc.set({
+            u'Count': 0,
+            u'Status': u'Closed',
+            u'Uptime': 0
+        })
+    transaction = db.transaction()
+    return occDoc, gateDoc, transaction
+
+
+# Set up transaction in the check_occupancy method
+@firestore.transactional
+def check_occupancy(transaction, doc):  # returns true if access is allowed and occupancy is increased, false otherwise
+    snapshot = doc.get(transaction=transaction)
+    if snapshot.get(u'Current') < snapshot.get(u'MaxOccupancy'):
+        transaction.update(doc, {
+            u'Current': snapshot.get(u'Current') + 1,
+            u'TotalIn': snapshot.get(u'TotalIn') + 1
+        })
+        return True
+    else:
+        return False
+
+
+def dec_occupancy(doc):
+    doc.update({
+        u'Current': firestore.Increment(-1)
+    })
+
+
+def dec_total(doc):
+    doc.update({
+        u'TotalIn': firestore.Increment(-1)
+    })
+
+
+def log_entrance(doc):
+    doc.update({
+        u'Count': firestore.Increment(1)
+    })
+
+
+def log_exit(doc):
+    doc.update({
+        u'Count': firestore.Increment(-1)
+    })
+
+
+def log_open(doc):
+    doc.update({
+        u'Status': u'Open'
+    })
+
+
+def log_close(doc):
+    doc.update({
+        u'Status': u'Closed'
+    })
+
+
+def log_emergency_open(doc):
+    doc.update({
+        u'Status': u'Emergency Mode'
+    })
+
+
+def log_time(doc, time):
+    doc.update({
+        u'Uptime': time
+    })
--- gate.py
+++ gate.py
@@ -1,57 +1,26 @@
 from gpiozero import MotionSensor
 from gpiozero import Button
 from gpiozero import Servo
-from google.cloud import firestore
-from google.oauth2 import service_account
 import time
-
+from datetime import date
+from dbinterface import *
 
 startTime = time.time()
+today = date.today().strftime("%b-%d-%Y")
+gateID = open('./id.txt', 'r').read()
+occDoc, gateDoc, transaction = db_init(today, gateID)
 
-# Set up firebase interaction
-cred = service_account.Credentials.from_service_account_file('./key.json')
-db = firestore.Client(u'thegate-608c2', cred)
-increment = firestore.Increment(1)
-decrement = firestore.Increment(-1)
-occDoc = db.collection(u'Counting').document(u'Occupancy')
-gateID = open('./id.txt','r').read()
-gateDoc = db.collection(u'Gates').document(u'Gate-'+gateID)
-if not gateDoc.get().exists:
-    gateDoc.set({
-        u'CountIn': 0,
-        u'CountOut': 0,
-        u'Status': u'Null',
-        u'Uptime': time.time() - startTime
-    })
 
 # Assign sensors and servo to GPIO pins on device
-pin = 1  # PLACEHOLDER, need to put in actual pin numbers once they're known
-entrTopSensor = MotionSensor(pin, queue_len=10, threshold=0.5)  # not sure if these params are good, we can test
-exitTopSensor = MotionSensor(pin)
-entrSideSensor = MotionSensor(pin)
-exitSideSensor = MotionSensor(pin)
-openSwitch = Button(hold_time=5)
-servo = Servo( )
-
-# Set up transaction in the check_occupancy method
-transaction = db.transaction()
-@firestore.transactional
-def check_occupancy(transaction):  # returns true if access is allowed and occupancy is increased, false otherwise
-    snapshot = occDoc.get(transaction=transaction)
-    if snapshot.get(u'curr') < snapshot.get(u'max'):
-        transaction.update(occDoc, {
-            u'curr': snapshot.get(u'curr') + 1
-        })
-        return True
-    else:
-        return False
+entrTopSensor = MotionSensor(2, queue_len=10, threshold=0.5)  # not sure if these params are good, we can test
+exitTopSensor = MotionSensor(16, queue_len=10, threshold=0.5)
+entrSideSensor = MotionSensor(4, queue_len=50, threshold=0.5)
+exitSideSensor = MotionSensor(21, queue_len=50, threshold=0.5)
+openSwitch = Button(22, hold_time=5)
+servo = Servo(8)
 
 
 # Define and assign methods to be performed when sensors detect motion
-def null_trigger():
-    pass
-
-
 def entr_sensor_triggered():
     entr_request()
 
@@ -71,52 +40,53 @@ forceOpen = False
 
 
 # Define other methods
-def open_gate():  # to be completed
+def open_gate(entering):  # to be completed
     global servo
-    servo.max()  # opens the gate
-    entrTopSensor.when_motion = null_trigger
-    exitTopSensor.when_motion = null_trigger
+    if entering:  # assumes that entering people need the servo to go to max
+        servo.max()
+    else:
+        servo.min()
+    entrTopSensor.when_motion = None
+    exitTopSensor.when_motion = None
+    log_open(gateDoc)
 
 
 def close_gate():  # to be completed
     global servo
-    servo.min()  # closes the gate
+    servo.mid()  # closes the gate
     entrTopSensor.when_motion = entr_sensor_triggered
     exitTopSensor.when_motion = exit_sensor_triggered
+    log_close(gateDoc)
 
 
 def entr_request():
-    if check_occupancy(transaction):  # this also increments the occupancy if it allows the person in
+    if check_occupancy(transaction, occDoc):  # this also increments the occupancy if it allows the person in
         print('access granted')
-        open_gate()
+        open_gate(True)
         exitSideSensor.wait_for_motion(7)  # they have 7 seconds to activate the side sensor on the exit side
         if not exitSideSensor.motion_detected:  # if the wait has timed out, i.e. they didn't enter
             close_gate()
-            occDoc.update({
-                u'curr': decrement
-            })
+            dec_occupancy(occDoc)
+            dec_total(occDoc)  # also need to decrease the TotalIn field
         else:  # if the person has begun passing through the gate
             exitSideSensor.wait_for_no_motion()  # no timeout because we don't want to close while someone is in the way
             close_gate()
-            log_entrance()
-
+            log_entrance(gateDoc)
     else:
         print('access denied, full')
         # handle sending data to screen
 
 
 def exit_request():
-    open_gate()
+    open_gate(False)
     entrSideSensor.wait_for_motion(7)  # they have 7 seconds to activate the side sensor on the entrance side
     if not entrSideSensor.motion_detected:
         close_gate()
     else:
         entrSideSensor.wait_for_no_motion()  # no timeout because we don't want to close while someone is in the way
         close_gate()
-        occDoc.update({  # we wait to decrement until we're sure it was a real exit
-            u'curr': decrement
-        })
-        log_exit()
+        dec_occupancy(occDoc)  # we wait to decrement until we're sure it was a real exit
+        log_exit(gateDoc)
 
 
 def emergency_open():
@@ -126,25 +96,16 @@ def emergency_open():
         forceOpen = False
         entrTopSensor.when_motion = entr_sensor_triggered
         exitTopSensor.when_motion = exit_sensor_triggered
+        log_emergency_open(gateDoc)
     else:
-        open_gate()
+        open_gate(False)  # same direction as exiting
         forceOpen = True
-        entrTopSensor.when_motion = null_trigger
-        exitTopSensor.when_motion = null_trigger
+        entrTopSensor.when_motion = None
+        exitTopSensor.when_motion = None
+        log_close(gateDoc)
 
 
-def log_entrance():
-    gateDoc.update({
-        u'CountIn': increment
-    })
-
-
-def log_exit():
-    gateDoc.update({
-        u'CountIn': decrement
-    })
-
-
-# Script body
+# Thread to continuously update uptime
 while True:
-    pass
+    log_time(gateDoc, time.time() - startTime)
+    time.sleep(5)
